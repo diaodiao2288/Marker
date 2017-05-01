3052c28f5b8491255af6ea28a6a22a1c05b3f614
ougwen1235 <ougwen1235@gmail.com>
2017-05-02 07:56:33 +0800

    初始化项目

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 ***
 SYSU Team Project
 
+## 开始
+```
+# 下载仓库
+git clone git@github.com:Abraham9511/Marker.git
+# 进入该仓库
+cd Marker
+# 安装依赖
+yarn install
+# 运行
+yarn run start
+```
+
 ## 分工
 * 前端：欧光文，林锦涛
 * 语法解析： 庄嘉鑫
@@ -10,16 +22,9 @@ SYSU Team Project
 * 界面设计：黄新伟
 
 ## 采用的技术
-* 前端：electronic
-* 语言：nodejs
+* 前端：electron
+* 语言：js
 
 ## 第一阶段
 5.12 完成第一次迭代，包括前端，语法解析
 5.14 生成pdf
-
-
-
-
-
-
-
---
+++ main.js
@@ -0,0 +1,43 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  // 在 OS X 上，通常用户在明确地按下 Cmd + Q 之前
+  // 应用会保持活动状态
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // 在 OS X 上，当用户点击dock图标
+  // 而没有打开的窗口时
+  // 将会自动创建一个窗口
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
