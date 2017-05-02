78fbc36b0d8a85227f90db25c9699567763557c8
ougwen1235 <ougwen1235@gmail.com>
2017-05-03 07:38:44 +0800

    添加eslint

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
 
-const path = require('path')
-const url = require('url')
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let mainWindow;
 
-let mainWindow
-
-function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
-    slashes: true
-  }))
+    slashes: true,
+  }));
 
   // mainWindow.webContents.openDevTools()
 
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   // 在 OS X 上，通常用户在明确地按下 Cmd + Q 之前
   // 应用会保持活动状态
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
-app.on('activate', function () {
+app.on('activate', () => {
   // 在 OS X 上，当用户点击dock图标
   // 而没有打开的窗口时
   // 将会自动创建一个窗口
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
